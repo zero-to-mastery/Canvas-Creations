65ba3787d4d6254e438d5feab942c5e5f1d48734
vivek patel <vivek.170410107085@gmail.com>
2020-10-01 18:37:01 +0530

    change js file

--- Art/vivek2509/machine_learning.js
+++ Art/vivek2509/machine_learning.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('mycanvas');
 const context = canvas.getContext('2d');
 
 
-// head
+// L_TRI
 context.beginPath();
 context.moveTo(0, 100);
 context.lineTo(0, 300);
@@ -10,7 +10,7 @@ context.lineTo(200, 200);
 context.fillStyle = 'white';
 context.fill();
 
-// head
+// R_TRY
 context.beginPath();
 context.moveTo(400, 200);
 context.lineTo(600, 100);
@@ -18,19 +18,21 @@ context.lineTo(600, 300);
 context.fillStyle = 'white';
 context.fill();
 
+// MIDDLE_block
+context.fillStyle = 'black';
+context.fillRect(280, 250, 35, 100);
 
-// text
+// TEXT
 context.font = '30px sans-serif';
 context.fillStyle = 'black';
 context.fillText('A world of', 230, 150);
 context.fillText('machine learning', 185, 185);
-
 context.font = '20px sans-serif';
 context.fillStyle = 'white';
 context.fillText('Welcome to Hacktoberfest! 2020', 160, 50);
 context.fillText('__________________________', 160, 50);
 
- //eyes
+//EYES
 context.beginPath();
 context.arc(145, 200, 7, 0, Math.PI * 2, true);
 context.fillStyle = 'black';
@@ -38,6 +40,3 @@ context.fill();
 context.beginPath();
 context.arc(445, 200, 7, 0, Math.PI * 2, true);
 context.fill();
-
-// legs
-context.fillRect(280, 250, 35, 100);
