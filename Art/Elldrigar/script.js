a289d18a407876a8402cefcf79def11593473699
Artur Gawron <artur@gawron.me>
2020-10-06 17:05:48 +0200

    Add basic animation

--- Art/Elldrigar/script.js
+++ Art/Elldrigar/script.js
@@ -2,12 +2,22 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let size = 0;
+let positionX = 0;
 
-context.fillStyle = '#dad5d5';
-context.strokeStyle = '#326ebb';
-context.lineWidth = 5;
-context.beginPath();
-context.arc(100, 300, 50, 0, Math.PI * 2);
-context.closePath();
-context.fill();
-context.stroke();
+const animate = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  size += 0.05;
+  positionX += 0.1;
+  context.fillStyle = '#dad5d5';
+  context.strokeStyle = '#326ebb';
+  context.lineWidth = 5;
+  context.beginPath();
+  context.arc(positionX, 300, size, 0, Math.PI * 2);
+  context.closePath();
+  context.fill();
+  context.stroke();
+  requestAnimationFrame(animate);
+};
+
+animate();
