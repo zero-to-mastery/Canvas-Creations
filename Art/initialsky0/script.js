f0c8bec9eefe17e44212104f2719bcde43c3e13a
initialsky0 <initialsky@protonmail.com>
2020-10-14 11:43:12 -0400

    add my art

--- Art/initialsky0/script.js
+++ Art/initialsky0/script.js
@@ -1,7 +1,19 @@
 class RainbowCircle {
    // Object for the canvas art
-   constructor(canvasId) {
+   constructor(canvasId, circleOrigin, incrementWidth) {
       this.ctx = this.initCanvas(canvasId);
+      this.origin = circleOrigin;
+      this.incWidth = incrementWidth;
+      this.rotateDeg = 0;
+      this.colorGrad = [
+         ['#ffa500', '#ff8300', '#ff0000'], 
+         ['#ffff00', '#ffcd00', '#ffa500'], 
+         ['#008000', '#80e800', '#ffff00'], 
+         ['#0000ff', '#009999', '#008000'], 
+         ['#4b0082', '#3415b0', '#0000ff'], 
+         ['#ee82ee', '#8506a9', '#4b0082'], 
+         ['#ff0000', '#d8005f', '#ee82ee']
+      ]
    }
 
    initCanvas(canvasId) {
@@ -34,27 +46,53 @@ class RainbowCircle {
       linGrad.addColorStop(1, grad3);
       this.ctx.fillStyle = linGrad;
    }
+
+   calcGradCoord(circRad, degRotate, clockwise) {
+      // calculate gradient coordinate based on circle radius and degree to rotate
+      const radius = circRad+this.incWidth;
+      let startCoordX, startCoordY, endCoordX, endCoordY;
+      // determine which direction to rotate
+      if(clockwise) {
+         startCoordX = this.origin[0]-(radius*Math.cos(degRotate*(Math.PI / 180)));
+         endCoordX = this.origin[0]+(radius*Math.cos(degRotate*(Math.PI / 180)));
+      } else {
+         startCoordX = this.origin[0]+(radius*Math.cos(degRotate*(Math.PI / 180)));
+         endCoordX = this.origin[0]-(radius*Math.cos(degRotate*(Math.PI / 180)));
+      }
+
+      startCoordY = this.origin[1]-(radius*Math.sin(degRotate*(Math.PI / 180)));
+      endCoordY = this.origin[1]+(radius*Math.sin(degRotate*(Math.PI / 180)));
+      return [startCoordX, startCoordY, endCoordX, endCoordY];
+   }
+
+   drawRainbow() {
+      // draw rainbow circles
+      const numOfCirc = Math.floor(this.origin[0] / this.incWidth);
+      let circRad = 0;
+      if(this.rotateDeg === 360) {
+         this.rotateDeg = 0;
+      }
+      for(let i = 0; i < numOfCirc; i ++) {
+         i ? this.ctx.restore() : this.ctx.save();
+         const gradColor = this.colorGrad[i % this.colorGrad.length];
+         const gradCoord = (i % 2) ? 
+            this.calcGradCoord(circRad, this.rotateDeg, true) : 
+            this.calcGradCoord(circRad, this.rotateDeg, false);
+         this.changeLinGrad3C(...gradColor, gradCoord);
+         const initDraw = i ? false : true;
+         this.drawCircle(this.origin, circRad+this.incWidth, circRad, initDraw);
+         circRad += this.incWidth;
+         this.rotateDeg++;
+      }
+   }
 }
 
 
 
-// practice draw
-function draw() {
-   const origin = [75, 75]
-   const rainCirc = new RainbowCircle('rainbow-circle');
-
-   rainCirc.ctx.save();
-   rainCirc.changeLinGrad3C('#0077b6', '#00b4d8', '#2b9348', [10, 0, 150, 0]);
-   rainCirc.drawCircle(origin, 75, 55);
-   
-
-   rainCirc.ctx.restore();
-   rainCirc.changeLinGrad3C('#2b9348', '#80b918', '#dddf00', [0, 10, 0, 150]);
-   rainCirc.drawCircle(origin, 55, 35);
-
-   rainCirc.ctx.restore();
-   rainCirc.changeLinGrad3C('#dddf00', '#ffb700', '#ff8800', [10, 0, 150, 0]);
-   rainCirc.drawCircle(origin, 35, 0, true);
+// draw rainbow circles
+function main() {
+   const rainCirc = new RainbowCircle('rainbow-circle', [250, 250], 20);
+   return setInterval(() => rainCirc.drawRainbow(), 50);
 }
 
-draw();
+main();
